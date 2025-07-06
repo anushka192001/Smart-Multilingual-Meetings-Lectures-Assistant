8c2eef27edf47e308e649b02077e98fcea54de26
anushka-cloduraai <63954873+anushka192001@users.noreply.github.com>
2025-07-07 00:45:32 +0530

    commit

--- docker/cpu/src/minutes_maker/__init__.py
+++
@@ -1,4 +0,0 @@
-from .minutes_maker import MinutesMaker
-
-__all__ = ["MinutesMaker"]
-__version__ = "0.1.0"
--- docker/cpu/view/minutes-maker/src/Components/ApiResponseDataSchema.ts
+++
@@ -1,7 +0,0 @@
-// Create a type for the API response data
-type ApiResponseDataSchema = {
-  timeline: string;
-  summary: string;
-};
-
-export type { ApiResponseDataSchema };
